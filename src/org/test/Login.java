343f0bde02a4cb22f30fc27dedd5688cc6c4bd83
lavanyaAarthi <lavanyaganesapandian@gmail.com>
2021-12-07 12:17:27 +0530

    Conflict Resolved

--- src/org/test/Login.java
+++ src/org/test/Login.java
@@ -2,22 +2,16 @@ package org.test;
 
 public class Login {
 	
-<<<<<<< HEAD
+    public static void userName() {
+		System.out.println("Username Fuunctionality");
+		}
+	
 	public static void passWord() {
 		System.out.println("password Fuunctionality");
 		}
 	
 	public static void main(String[] args) {
+		userName();
 		passWord();
 	}
- }
-=======
-	public static void userName() {
-		System.out.println("Username Fuunctionality");
-		}
-	
-	public static void main(String[] args) {
-		userName();
-	}
 }
->>>>>>> 73caabbce2ff1ea8075a460b2598a200d6cad410
