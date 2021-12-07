f20f473f096f1000c7a77919150713dcf559f105
lavanyaAarthi <lavanyaganesapandian@gmail.com>
2021-12-07 11:05:30 +0530

    Password Functionality

--- src/org/test/Login.java
+++ src/org/test/Login.java
@@ -1,5 +1,12 @@
 package org.test;
 
 public class Login {
-
-}
+	
+	public static void passWord() {
+		System.out.println("password Fuunctionality");
+		}
+	
+	public static void main(String[] args) {
+		passWord();
+	}
+ }
